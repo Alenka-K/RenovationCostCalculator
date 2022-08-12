b7eeb75b78b5ed434b9e3907bd1cb749ac318556
Elena K <alenkaksa81@gmail.com>
2022-08-12 15:27:16 +0300

    Initial commit

---
+++ src/main/java/com/example/renovationcostcalculator/RenovationCostCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.renovationcostcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovationCostCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenovationCostCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/renovationcostcalculator/RenovationCostCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.renovationcostcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovationCostCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
